7ce72ea28fee562aea50ecd181272ee8e4b85478
aryainguz <aryaninguz369@gmail.com>
2025-11-30 23:55:44 +0530

    init: boiler plate

---
+++ src/test/java/com/inguzdev/gitly/GitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inguzdev.gitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
